0c834443d8f4320df35d834f251ded0d2d9be813
vaibhavswami13 <vaibhavswami14061995@gmail.com>
2026-02-17 14:20:01 +0530

    script

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "(print hello)"
+echo "current user:$(whoami)"
+echo "Today's date:$(date)"
+echo "hostname:$(hostname)"
+echo "working directory:$(pwd)"
+echo "list items:$(ls)"
+
